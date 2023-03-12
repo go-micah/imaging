edb76a7728919505dede9b3ee12b03f3c3c3d322
Micah Walter <47419+micahwalter@users.noreply.github.com>
2023-03-12 19:23:02 -0400

    added another case to test

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -4,9 +4,10 @@ import "testing"
 
 func TestAbs(t *testing.T) {
 	want := 100
-	got := Abs(-100)
+	got_pos := Abs(100)
+	got_neg := Abs(-100)
 
-	if want != got {
-		t.Errorf("expected %q, got %q", want, got)
+	if (want != got_pos) || (want != got_neg) {
+		t.Errorf("expected %q, got %q and %q", want, got_pos, got_neg)
 	}
 }
